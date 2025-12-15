e1e09e51f7fdd35dbd63f44a36f99a541eb537d3
onuma <a.onuma@1st-scratch.co.jp>
2025-12-15 11:22:57 +0900

    first commit

---
+++ weatherAppSwift/ui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weatherAppSwift
+//
+//  Created by 大沼　 on 2025/12/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weatherAppSwift/weatherAppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppSwiftApp.swift
+//  weatherAppSwift
+//
+//  Created by 大沼　 on 2025/12/12.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
