9607801769fb2ddcaaf6426c92885e54f63eb6b1
onuma <a.onuma@1st-scratch.co.jp>
2025-12-25 19:09:56 +0900

    Fix URL Send function

--- weatherAppSwift/data/repository/WeatherAPIService.swift
+++ weatherAppSwift/data/repository/WeatherAPIService.swift
@@ -20,25 +20,46 @@ protocol WeatherApiServiceProtcol {
  天気情報取得API
  */
 class WeatherApiService: WeatherApiServiceProtcol {
-    
     // 天気情報取得APIを実行
     func fetchWeather(city: String) async throws -> Data {
+
         // API実行URLを設定
-        let baseUrl = Bundle.main.object(forInfoDictionaryKey: "ENV_API_ENDPOINT") as? String
-        
-        // API実行URLが異常な場合
-        if baseUrl == nil || baseUrl == "" {
+        guard let baseUrl = Bundle.main.object(forInfoDictionaryKey: "ENV_API_ENDPOINT") as? String,
+              !baseUrl.isEmpty,
+              // URL形式のオブジェクトを設定
+              var urlComponents = URLComponents(string: baseUrl)
+        else {
             throw URLError(.badURL)
         }
-        
-        // URL形式ではなかった場合
-        guard let baseUrlString = baseUrl, let url = URL(string: baseUrlString + city) else {
+
+        // APIキーを取得
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String,
+                !apiKey.isEmpty
+        else {
             throw URLError(.badURL)
         }
+
+        // パラメータを設定（q = 地域名）
+        var queries : [String: String] = [
+            "appid": apiKey,
+            "lang": "ja",
+            "units":"metric",
+            "q" : city,
+        ]
         
+        urlComponents.queryItems =
+            queries.map {
+                URLQueryItem(name: $0.key, value: $0.value)
+            }
+
+        // URLを取得
+        guard let url = urlComponents.url else {
+            throw URLError(.badURL)
+        }
+
         // API実行（URLセッション）
         let (data, response) = try await URLSession.shared.data(from: url)
-        
+
         //レスポンスチェック
         guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
             throw URLError(.badServerResponse)
